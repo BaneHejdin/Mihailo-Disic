be7b793cdb47e63e70d86609f1c7deee6be14544
BaneHejdin <153732341+BaneHejdin@users.noreply.github.com>
2023-12-20 12:48:31 +0100

    Add files via upload

---
+++ izvestaj.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace rad_za_ocenu
+{
+    public partial class izvestaj : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(PreviousPage != null)
+                lbl_izv.Text=PreviousPage.ime + " ("+ PreviousPage.email + ") , rodjen " + PreviousPage.god + " . godine, učenik "
+                + PreviousPage.raz + " razreda , uspešno je popunio obrazac za prijavu. ";
+        }
+    }
+}
